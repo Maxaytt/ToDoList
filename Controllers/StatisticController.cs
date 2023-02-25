d41fd28f922fe70343943d882b77628e9e668a14
PiskunovMykola <118397025+PiskunovMykola@users.noreply.github.com>
2023-02-25 22:19:28 +0100

    Add StatisticController Add Index

---
+++ Controllers/StatisticController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using ToDo;
+
+namespace TodoList.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        public StatisticController(AppDbContext context, UserManager<IdentityUser> userManager) 
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: TodoTasks
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var todoTasks = await _context.TodoTasks
+                .Where(t => t.UserId == userId)
+                .ToListAsync();
+
+            return View(todoTasks);
+        }
+        
+    }
+}

192f78b022d984a0dd9cf8b31af2b27cb9d8d139
Maxaytt <maxaytt@gmail.com>
2023-03-02 17:33:02 +0100

    Create table in index add taskCategory model

---
+++ Models/TaskCategory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Models
+{
+    public class TaskCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Range(1, 20)]
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
